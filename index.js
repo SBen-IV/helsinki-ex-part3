4f3118030602357ab7d74e19b2a5f1bf6c91e7e0
Sebastian Bento Inneo Veiga <sinneo@fi.uba.ar>
2022-03-08 20:07:53 -0300

    update part3c

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
 const cors = require('cors')
+const Person = require('./models/person')
 
 app.use(cors())
 app.use(express.static('build'))
@@ -31,23 +33,17 @@ let persons = [
 	}
 ]
 
-const generateID = () => {
-	return Math.floor(Math.random() * 1000)
-}
-
 app.get('/api/persons', (request, response) => {
-	response.json(persons)
+	Person.find({}).then(persons => {
+		response.json(persons)
+	})
 })
 
 app.get('/api/persons/:id', (request, response) => {
-	const id = Number(request.params.id)
-	const person = persons.find(p => p.id === id)
-
-	if (person) {
+	Person.findById(request.params.id).then(person => {
 		response.json(person)
-	} else {
-		response.status(404).end()
-	}
+	})
+	.catch(error => next(error))
 })
 
 app.post('/api/persons', (request, response) => {
@@ -59,36 +55,59 @@ app.post('/api/persons', (request, response) => {
 		})
 	}
 
-	if (persons.find(person => person.name === body.name)) {
-		return response.status(400).json({
-			error: "Name must be unique."
+	const person = new Person({
+		name: body.name,
+		number: body.number,
+	})
+
+	person.save().then(savedPerson => {
+		response.json(savedPerson)
+	})
+})
+
+app.delete('/api/persons/:id', (request, response, next) => {
+	Person.findByIdAndRemove(request.params.id)
+		.then(result => {
+			response.status(204).end()
 		})
-	}
+		.catch(error => next(error))
+})
+
+app.put('/api/persons/:id', (request, response, next) => {
+	const body = request.body
 
 	const person = {
 		name: body.name,
-		number: body.number,
-		id: generateID()
+		number: body.number
 	}
 
-	persons = persons.concat(person)
-
-	response.json(person)
-})
-
-app.delete('/api/persons/:id', (request, response) => {
-	const id = Number(request.params.id)
-	persons = persons.filter(p => p.id !== id)
-
-	response.status(204).end()
+	Person.findByIdAndUpdate(request.params.id, person, { new: true })
+		.then(updatedPerson => {
+			response.json(updatedPerson)
+		})
+		.catch(error => next(error))
 })
 
 app.get('/info', (request, response) => {
-	const message = `Phonebook has info for ${persons.length} people`
-	const date = new Date()
+	Person.find({}).count((error, count) => {
+		const message = `Phonebook has info for ${count} people`
+		const date = new Date()
 
-	response.send(`${message}<br/><br/>${date}`)
+		response.send(`${message}<br/><br/>${date}`)
+	})
 })
+
+const errorHandler = (error, request, response, next) => {
+	console.error(error.message)
+
+	if (error.name === 'CastError') {
+		return response.status(400).send({ error: 'malformatting id' })
+	}
+
+	next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
